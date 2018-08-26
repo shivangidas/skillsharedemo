aa0eadb69e660f01d6f759e3bfbd451dd1dfc135
shivangi <shivangi.das7@gmail.com>
2018-08-26 21:32:47 +0530

    added a new test route

--- routes/route.js
+++ routes/route.js
@@ -5,7 +5,9 @@ module.exports = function(app){
     app.get('/', (req, res) => {
         res.send('Hello, World!');
     });
-    
+    app.get('/test', (req, res) => {
+        res.send({message: 'testing'});
+    });
     app.get('/image', (req, res) => {
         res.sendFile(path.join(__dirname , "../public/books.jpg"));
     });
