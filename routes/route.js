3f29109b64abf662d4420e06c9b470fd60a15b06
shivangi <shivangi.das7@gmail.com>
2018-08-26 21:27:56 +0530

    'redo'

--- routes/route.js
+++ routes/route.js
@@ -5,9 +5,7 @@ module.exports = function(app){
     app.get('/', (req, res) => {
         res.send('Hello, World!');
     });
-    app.get('/test', (req, res) => {
-        res.send({message: 'testing'});
-    });
+    
     app.get('/image', (req, res) => {
         res.sendFile(path.join(__dirname , "../public/books.jpg"));
     });
