77a0503bc80141fdeae1e3a500aaf155edf8a1ce
shivangi <shivangi.das7@gmail.com>
2018-08-28 00:11:40 +0530

    added an html file

--- routes/route.js
+++ routes/route.js
@@ -2,13 +2,17 @@
 const path = require('path');
 
 module.exports = function(app){
+    //text
     app.get('/', (req, res) => {
         res.send('Hello, World!');
     });
-    app.get('/test', (req, res) => {
-        res.sendFile(path.join(__dirname , "../public/index.html"));
-    });
+    //image
     app.get('/image', (req, res) => {
         res.sendFile(path.join(__dirname , "../public/books.jpg"));
     });
+    //html page
+    app.get('/test', (req, res) => {
+        res.sendFile(path.join(__dirname , "../public/index.html"));
+    });
+
 }
